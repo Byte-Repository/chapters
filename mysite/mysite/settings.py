896cbc031c529c6eb81d6cd8d022875e600b53a6
cesar <cesarortegafrias@gmail.com>
2024-09-12 17:54:09 -0500

    Assignmnet 0 Commit #4

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -28,8 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-SITE_ID = 1
-
 
 # Application definition
 
@@ -83,11 +81,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
