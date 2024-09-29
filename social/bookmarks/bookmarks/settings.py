39f82bce6f69998da5dac020dd760a9248611c8c
cesar <cesarortegafrias@gmail.com>
2024-09-29 00:06:11 -0500

    Chapter 4 Commit #4.2

--- social/bookmarks/bookmarks/settings.py
+++ social/bookmarks/bookmarks/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
