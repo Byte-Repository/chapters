4720dc2f1b41146254a6fa603ffd3db6a0a3d568
cesar <cesarortegafrias@gmail.com>
2024-09-30 16:46:18 -0500

    Chapter 5 Settings Fix Commit #5.1

--- social/bookmarks/bookmarks/settings.py
+++ social/bookmarks/bookmarks/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'bookmarks.urls'
